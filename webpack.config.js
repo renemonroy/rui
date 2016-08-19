9c1c306c7f3270013a204f9668a0bb55e3a7b108
René Monroy <rene.monroy@gmail.com>
2016-08-18 23:31:57 -0500

    Use webpack to create dev files

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,61 @@
 var webpack = require('webpack');
 var envIsDev = process.env && process.env.NODE_ENV == 'development';
+var path = require('path');
 
 var getEntry = function() {
   var entry = [];
-  if ( envIsDev ) {
-    entry.push(
-      'webpack-dev-server/client?http://localhost:3000',
-      'webpack/hot/only-dev-server'
-    );
-  }
+  // if ( envIsDev ) {
+  //   entry.push(
+  //     'webpack-dev-server/client?http://localhost:3000',
+  //     'webpack/hot/only-dev-server'
+  //   );
+  // }
   entry.push('./src/scripts/index.jsx');
   return entry;
 };
 
 module.exports = {
 
-  entry : getEntry(),
+  entry: getEntry(),
 
-  output : {
-    path : __dirname + '/dist',
-    publicPath : '/',
-    filename : 'bundle.js'
+  output: {
+    path: path.join(__dirname, envIsDev ? 'lib' : 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
 
-  devServer : {
-    contentBase : './dist',
-    hot : true
+  devServer: {
+    contentBase: envIsDev ? './lib' : './dist',
+    hot: true,
   },
 
-  module : {
-    loaders : [
+  module: {
+    loaders: [
       {
-        test : /\.jsx?$/,
-        exclude : /node_modules/,
-        loader : envIsDev ? 'babel' : 'react-hot!babel'
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: envIsDev ? 'babel' : 'react-hot!babel',
       },
       {
-        test : /\.styl$/,
-        loader : 'style-loader!css-loader!stylus-loader'
+        test: /\.styl$/,
+        loader: 'style-loader!css-loader!stylus-loader',
       },
       {
-        test : /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader : 'url-loader?limit=100000'
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000',
       },
       {
         test: /\.(png|jpg)$/,
-        loader: 'file-loader?name=images/[name].[ext]'
+        loader: 'file-loader?name=images/[name].[ext]',
       },
-    ]
+    ],
   },
 
-  resolve : {
-    extensions : ['', '.js', '.jsx']
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
   },
 
-  plugins : [
+  plugins: [
     new webpack.HotModuleReplacementPlugin(),
   ],
 
